c9c477d84ce50c709219d6189bbb41eaf01a1bce
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-22 23:35:15 +0530

    boilerplate

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,3 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract MyEpicNFT {
+    constructor() {
+        console.log("This is a constructor");
+    }
+}
