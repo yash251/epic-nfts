b8fd5c8ac1d4716075cdb204c0dd890bb40fcb2f
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-23 19:18:55 +0530

    NFT function

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,11 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.4;
-
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor() {
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract MyEpicNFT is ERC721URIStorage {
+    // keep track of tokenIds
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // We need to pass the name of our NFTs token and its symbol.
+    constructor() ERC721 ("SquareNFT", "SQUARE") {
         console.log("This is a constructor");
+    }
+
+    // a function our user will hit to get their NFT
+    function makeAnEpicNFT() public {
+        // get the current tokenId, this starts at 0
+        uint256 newItemId = _tokenIds.current();
+
+        // actually mint the NFT to the sender using msg.sender
+        _safeMint(msg.sender, newItemId);
+
+        // set the NFTs data
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/RUUS");
+        console.log("An NFT with ID %s has been minted to %s", newItemId, msg.sender);
+
+
+        // increment the counter for when the next NFT is minted
+        _tokenIds.increment();
     }
 }
