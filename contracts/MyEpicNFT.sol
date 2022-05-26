525c3045a8de748c6034680395d85b7ea60c4ce7
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-26 22:58:44 +0530

    setTokenURI to encoded json

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -27,7 +27,7 @@ contract MyEpicNFT is ERC721URIStorage {
         _safeMint(msg.sender, newItemId);
 
         // set the NFTs data
-        _setTokenURI(newItemId, "https://jsonkeeper.com/b/RUUS");
+        _setTokenURI(newItemId, "data:application/json;base64,ewogICAgIm5hbWUiOiAiWWFzaEJsb2NrY2hhaW5Gb3JldmVyIiwKICAgICJkZXNjcmlwdGlvbiI6ICJBbiBORlQgZnJvbSB0aGUgaGlnaGx5IGFjY2xhaW1lZCBZYXNoIGNvbGxlY3Rpb24iLAogICAgImltYWdlIjogImRhdGE6aW1hZ2Uvc3ZnK3htbDtiYXNlNjQsUEhOMlp5QjRiV3h1Y3owaWFIUjBjRG92TDNkM2R5NTNNeTV2Y21jdk1qQXdNQzl6ZG1jaUlIQnlaWE5sY25abFFYTndaV04wVW1GMGFXODlJbmhOYVc1WlRXbHVJRzFsWlhRaUlIWnBaWGRDYjNnOUlqQWdNQ0F6TlRBZ016VXdJajRLSUNBZ0lEeHpkSGxzWlQ0dVltRnpaU0I3SUdacGJHdzZJSGRvYVhSbE95Qm1iMjUwTFdaaGJXbHNlVG9nYzJWeWFXWTdJR1p2Ym5RdGMybDZaVG9nTVRSd2VEc2dmVHd2YzNSNWJHVStDaUFnSUNBOGNtVmpkQ0IzYVdSMGFEMGlNVEF3SlNJZ2FHVnBaMmgwUFNJeE1EQWxJaUJtYVd4c1BTSmliR0ZqYXlJZ0x6NEtJQ0FnSUR4MFpYaDBJSGc5SWpVd0pTSWdlVDBpTlRBbElpQmpiR0Z6Y3owaVltRnpaU0lnWkc5dGFXNWhiblF0WW1GelpXeHBibVU5SW0xcFpHUnNaU0lnZEdWNGRDMWhibU5vYjNJOUltMXBaR1JzWlNJK1dXRnphRUpzYjJOclkyaGhhVzVHYjNKbGRtVnlQQzkwWlhoMFBnbzhMM04yWno0PSIKfQ=="); // not using jsonkeeper anymore as it may go down, instead encoded the json file having SVG image
         console.log("An NFT with ID %s has been minted to %s", newItemId, msg.sender);
 
 
