5cdd1ab9bc12c17576edef021720d4c722dc5297
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-26 23:36:10 +0530

    svg and array strings

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.4;
 // We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/utils/Strings.sol"; // function for strings
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
@@ -12,6 +13,15 @@ contract MyEpicNFT is ERC721URIStorage {
     // keep track of tokenIds
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    // This is our SVG code. All we need to change is the word that's displayed. Everything else stays the same.
+    // So, we make a baseSvg variable here that all our NFTs can use.
+    string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
+
+    // arrays for generating three random words
+    string[] firstWords = ["DeBruyne", "Grealish", "Foden", "Mahrez", "Gundogan", "Fernandinho", "Sterling", "Cancelo", "Jesus", "Zinchenko", "Rodri", "Silva", "Walker", "Ederson", "Laporte"];
+    string[] secondWords = ["Barney", "Ted", "Joey", "Ross", "Chandler", "Sheldon", "Leonard", "Howard", "Raj", "Harvey", "Mike", "Walter", "Stefan", "Otis", "Tommy"];
+    string[] thirdWords = ["Kendall", "Ariana", "Alia", "Deepika", "Milley", "Khushi", "Emma", "Margot", "Angelina", "Selena", "Rihanna", "Camila", "Beyonce", "Billie", "Dua"];
 
     // We need to pass the name of our NFTs token and its symbol.
     constructor() ERC721 ("SquareNFT", "SQUARE") {
